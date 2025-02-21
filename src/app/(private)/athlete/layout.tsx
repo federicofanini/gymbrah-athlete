41f107e37cb9fed823f6e87763d75fadec8fc659
federicofanini <federico.fanini9@gmail.com>
2025-02-21 10:57:23 +0100

    minor

--- src/app/(private)/athlete/layout.tsx
+++ src/app/(private)/athlete/layout.tsx
@@ -23,7 +23,7 @@ export default async function DashboardLayout({
 
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
-    redirect("/login");
+    redirect("/");
   }
 
   return (
